b6cd8ca2921cc1573a9ef676b66ad4a6d58e2ae6
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-04-09 01:24:04 -0300

    Parte 4|passo 2

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/DisplayMessageActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/DisplayMessageActivity.java
@@ -2,6 +2,7 @@ package br.edu.ifpe.tads.pdm.pratica01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        Intent intent =getIntent();
+        String message =intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
     }
 }
