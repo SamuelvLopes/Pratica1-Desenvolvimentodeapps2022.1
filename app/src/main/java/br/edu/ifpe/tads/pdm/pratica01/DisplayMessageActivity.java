20d66a5302a434030795c5e140784272ce36112a
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-04-09 01:25:36 -0300

    Parte 4|passo 3 E 4

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/DisplayMessageActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/DisplayMessageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -13,5 +14,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
         Intent intent =getIntent();
         String message =intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = (TextView)findViewById(R.id.message_view);
+        textView.setTextSize(40);
+        textView.setText(message);
     }
 }
