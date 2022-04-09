9e1213d4bb8359aaec1ddc77b808bea59108d368
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-04-09 01:49:38 -0300

    Parte 5|Passo 2

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/MainActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica01/MainActivity.java
@@ -4,6 +4,8 @@ import static android.content.ClipData.newIntent;
 
 import android.content.Intent;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,4 +28,24 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE,message);
         startActivity(intent);
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);return true;
+    }
+    @Overridepublic boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+            switch (item.getItemId()) {
+                case R.id.action_search:
+                    openSearch();
+                    return true;
+                case R.id.action_settings:
+                    openSettings();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+
+
+}
 }
